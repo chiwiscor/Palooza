0c68101a50a70283dcf97a14b045497cc7f7389e
jose luis luna rubio <chiwiscor@joseluislunarubio.net>
2016-10-09 02:24:13 -0500

    se agrego el offCanvas

--- js/app.js
+++ js/app.js
@@ -5,3 +5,30 @@ $("[data-toggle-dia]").click(function(ev){
   console.log(panel)
   $("#lineup-tabs").foundation("selectTab",panel)
 })
+
+const $menuOff = $("#menuOff")
+const $body = $("body")
+const $contenedorCanvas=$(".js-off-canvas-exit")
+
+$contenedorCanvas.click(function(eve){
+  $body.addClass("fondo-imagen").removeClass("fondo-solido");
+})
+
+//para cuando se da click en el item del offCanvas
+const $offCanvas = $("#offCanvas")
+const $sticky = $("#data-sticky")
+
+$offCanvas.find('li').click(function(ev){
+  $body.addClass("fondo-imagen").removeClass("fondo-solido");
+  $offCanvas.foundation('close')
+  setTimeout(function(){
+    $sticky.css('left','0px')
+  }, 300)
+
+})
+
+
+//para agregar y/o quitar css al body
+$menuOff.click(function(ev){
+  $body.addClass("fondo-solido").removeClass("fondo-imagen");
+})
